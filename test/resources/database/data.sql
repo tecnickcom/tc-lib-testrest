74b74c867dbf5d141845592d7c556086658ef655
nicolaasuni <info@tecnick.com>
2016-05-02 13:44:27 +0100

    removing multiple inserts in test for php 5.4 compatibility

--- test/resources/database/data.sql
+++ test/resources/database/data.sql
@@ -1,6 +1,8 @@
 -- test data
 -- Nicola Asuni - 2015-09-11
 
-INSERT INTO test (name) VALUES ('alpha'),('beta'),('gamma');
+INSERT INTO test (name) VALUES ('alpha');
+INSERT INTO test (name) VALUES ('beta');
+INSERT INTO test (name) VALUES ('gamma');
 ;
 ;
